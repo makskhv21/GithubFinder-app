351cbad4477e418cca9cc5c4053ad7f1ea1f6ed6
repo <khvits21@gmail.com>
2024-07-14 15:28:34 +0300

    style: Update Profile with API integration

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,24 +1,60 @@
 import styled from "styled-components"
+import locationIcon from "../assets/locationIcon.svg";
+import websiteIcon from "../assets/websiteIcon.svg";
+import twwiterIcon from "../assets/twitterIcon.svg"
+import companyIcon from "../assets/companyIcon.svg"
 import moment from 'moment';
 
 export default function Profile(props: HeaderProps & UserInfoProps) {
-    const createdAt = props.userInfo?.created_at;
-    const formattedDate = moment(createdAt).format('DD MMM YYYY');
+
+    const formattedDate = moment(props.userInfo?.created_at).format('DD MMM YYYY');
     
     return (
         <ProfileContainer isDark={props.isDark}>
-        <div className="userInfo">
+          <div className="userInfo">
             <img src={props.userInfo?.avatar_url} alt="userAvatar" />
             <div className="mainInfo">
-            <p className="name">{props.userInfo?.name}</p>
-            <p className="login">@{props.userInfo?.login}</p>
-            <p className="createdAt">Joined {formattedDate}</p>
+              <p className="name">{props.userInfo?.name}</p>
+              <p className="login">@{props.userInfo?.login}</p>
+              <p className="createdAt">Joined {formattedDate}</p>
             </div>
-        </div>
-        <p>{props.userInfo?.bio}</p>
+          </div>
+          <p>{props.userInfo?.bio}</p>
+          <div className="followers">
+            <div className="infoBox">
+              <p className="statNme">Repos</p>
+              <p className="stat">{props.userInfo?.public_repos}</p>
+            </div>
+            <div className="infoBox">
+              <p className="statNme">Followers</p>
+              <p className="stat">{props.userInfo?.followers}</p>
+            </div>
+            <div className="infoBox">
+              <p className="statNme">Following</p>
+              <p className="stat">{props.userInfo?.following}</p>
+            </div>
+          </div>
+          <div className="wrapper">
+            <div className="wrapperInfo">
+              <img src={locationIcon} alt="locationIcon" />
+              <p>{props.userInfo?.location}</p>
+            </div>
+             <div className="wrapperInfo">
+              <img src={websiteIcon} alt="websiteIcon" />
+              <p>{props.userInfo?.blog}</p>
+            </div>
+             <div className="wrapperInfo">
+              <img src={twwiterIcon} alt="websiteIcon" />
+              <p>{props.userInfo?.twitter_username}</p>
+            </div>
+             <div className="wrapperInfo">
+              <img src={companyIcon} alt="companyIcon" />
+              <p>{props.userInfo?.company}</p>
+            </div>
+          </div>
         </ProfileContainer>
     );
-};
+}
 
 const ProfileContainer = styled.div<{ isDark: boolean }>`
   width: 328px;
@@ -60,9 +96,54 @@ const ProfileContainer = styled.div<{ isDark: boolean }>`
     color: #0079ff;
   }
   .createdAt {
+font-size: 13px;
+    line-height: 19px;
+    color: ${(props) => (props.isDark ? "#FFFFFF" : "#697C9A")};
+  }
+  .followers {
+    width: 280px;
+    height: 85px;
+    display: flex;
+    gap: 28px;
+    border-radius: 10px;
+    background: ${(props) => (props.isDark ? "#141D2F" : "#F6F8FF")};
+    margin: 24px 0;
+    padding: 20px 0 0 40px;
+  }
+  .infoBox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .statNme {
+    font-weight: 400;
+    font-size: 11px;
+    line-height: 16px;
+    color: ${(props) => (props.isDark ? "#FFFFFF" : "#4B6A9B")};
+  }
+  .stat {
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 24px;
+    color: ${(props) => (props.isDark ? "#FFFFFF" : "#2B3442")};
+  }
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 18px;
+  }
+  .wrapperInfo {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+  }
+  .wrapperInfo img {
+    filter: ${(props)=> (props.isDark ? "invert(100%) sepia(0%) saturate(0%) hue-rotate(326deg) brightness(1000%) contrast(102%)" : null)} ;
+  }
+  .wrapperInfo p {
     font-weight: 400;
     font-size: 13px;
     line-height: 19px;
-    color: ${(props) => (props.isDark ? "#FFFFFF" : "#697C9A")} ;
+    color: ${(props) => (props.isDark ? "#FFFFFF" : "#4b6a9b")}; 
   }
 `;
